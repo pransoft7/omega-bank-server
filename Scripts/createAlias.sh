f05f55c6d75d755a56535fcece2d6c5a72d28973
Pranava S Kumar <95279546+pransoft7@users.noreply.github.com>
2022-06-12 17:03:36 +0530

    Update createAlias.sh

--- Scripts/createAlias.sh
+++ Scripts/createAlias.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo alias genUser=\'bash genUser.sh\' >> .bashrc
+echo alias permit=\'bash permit.sh\' >> .bashrc
 echo alias updateBranch=\'bash updateBranch.sh\' >> .bashrc
 echo alias allotInterest=\'bash allotInterest.sh\' >> .bashrc
 echo alias makeTransaction=\'bash makeTransaction.sh\' >> .bashrc
